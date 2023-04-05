53dcc3ba279d72b3ac7b271327c61a43c5e550a7
w5b <minecraftofer@gmail.com>
2023-04-05 17:54:28 +0300

    easier to read

--- findMopeVariables.js
+++ findMopeVariables.js
@@ -1,19 +1,24 @@
-
 const string = require('./gameClient.js')
 let devModEtc = {
   rgx: new RegExp(/= 0x0,[\n\s]+\w+ = !\[\].\n\w+ = !\[\].[\s\S\n]*?gCanvas/g),
   names: [
-    " //isSuperDev\n",
-    " //devMode_num\n",
-    " //devMode_zoomEnabled\n",
-  ]
+    " //isSuperDev",
+    " //devMode_num",
+    " //devMode_zoomEnabled",
+  ],
+  key: 0,
+  start: 1,
+  amount: 3,
 }
 
 let playGame = {
   rgx: new RegExp(/function \w+\(\w+\) \{\n!/g),
   names: [
-    "// playGame function",
-  ]
+    " //playGame function",
+  ],
+  key: 0,
+  start: 0,
+  amount: 1,
 }
 
 function findRegex(rgx, objectKey, start, amount, names, filter = null) {
@@ -24,17 +29,17 @@ function findRegex(rgx, objectKey, start, amount, names, filter = null) {
   if (filter) {
     let variables = rgxObjectKeys.match(filter);
     for (let i = start; i < start + amount; i++) {
-      finalString.push(variables[i].replace("\n", ""));
+      finalString.push(variables[i]);
     }
     for (let i = 0; i < names.length; i++) {
       finalString[i] += names[i];
     }
   }
   else {
-    finalString.push(rgxObjectKeys.replace("\n", "") + names[0]);
+    finalString.push(rgxObjectKeys + names[0]);
   }
   return finalString;
 }
 
-console.log(findRegex(devModEtc.rgx, 0, 1, 3, devModEtc.names, /_\w+/g));
-console.log(findRegex(playGame.rgx, 0, 0, 1, playGame.names));
+console.log(findRegex(devModEtc.rgx, devModEtc.key, devModEtc.start, devModEtc.amount, devModEtc.names, /_\w+/g));
+console.log(findRegex(playGame.rgx, playGame.key, playGame.start, playGame.amount, playGame.names));
